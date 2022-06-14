756f3eeb35209704ac0055e7eccf486cc64dd9ec
Effencee <107486971+Effencee@users.noreply.github.com>
2022-06-14 16:58:30 +0200

    adding files

---
+++ main.js
@@ -0,0 +1,24 @@
+let size = 10;
+let orderElement = 1;
+
+const init = () => {
+    const btn = document.createElement('button');
+    btn.textContent = 'Dodaj 10 elementów listy';
+    document.body.appendChild(btn);
+    const ul = document.createElement('ul');
+    document.body.appendChild(ul);
+    btn.addEventListener('click', createLiElements);
+}
+
+const createLiElements = () => {
+ // tutaj kod
+ for (let i = 0; i < size; i++) {
+    const liItems = document.createElement('li'); 
+    liItems.textContent = 'element ' + orderElement;
+    liItems.style.fontSize = orderElement + 10 +'px';
+    orderElement++; 
+    document.body.querySelector('ul').appendChild(liItems);
+ }
+}
+
+init()
